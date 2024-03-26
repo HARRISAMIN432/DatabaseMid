1dd094e7b8ca7334eeeac40d946a4fbe601135fc
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2024-03-27 01:04:16 +0500

    DB

---
+++ DBMidProject/MidProject/Home.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace MidProject
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.DisabledLinkColor = System.Drawing.Color.RoyalBlue;
+			this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+			this.linkLabel1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.linkLabel1.LinkColor = System.Drawing.Color.BlueViolet;
+			this.linkLabel1.Location = new System.Drawing.Point(489, 122);
+			this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(86, 13);
+			this.linkLabel1.TabIndex = 0;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "Manage Student";
+			this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(489, 197);
+			this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(70, 13);
+			this.linkLabel2.TabIndex = 1;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "Manage CLO";
+			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+			// 
+			// linkLabel3
+			// 
+			this.linkLabel3.AutoSize = true;
+			this.linkLabel3.Location = new System.Drawing.Point(489, 273);
+			this.linkLabel3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel3.Name = "linkLabel3";
+			this.linkLabel3.Size = new System.Drawing.Size(85, 13);
+			this.linkLabel3.TabIndex = 2;
+			this.linkLabel3.TabStop = true;
+			this.linkLabel3.Text = "Manage Rubrics";
+			this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+			// 
+			// linkLabel4
+			// 
+			this.linkLabel4.AutoSize = true;
+			this.linkLabel4.Location = new System.Drawing.Point(489, 347);
+			this.linkLabel4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel4.Name = "linkLabel4";
+			this.linkLabel4.Size = new System.Drawing.Size(105, 13);
+			this.linkLabel4.TabIndex = 3;
+			this.linkLabel4.TabStop = true;
+			this.linkLabel4.Text = "Manage Assessment";
+			this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+			// 
+			// linkLabel5
+			// 
+			this.linkLabel5.AutoSize = true;
+			this.linkLabel5.Location = new System.Drawing.Point(489, 440);
+			this.linkLabel5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel5.Name = "linkLabel5";
+			this.linkLabel5.Size = new System.Drawing.Size(96, 13);
+			this.linkLabel5.TabIndex = 4;
+			this.linkLabel5.TabStop = true;
+			this.linkLabel5.Text = "Manage Rubric Co";
+			this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(398, 25);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(321, 31);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Lab Management System";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Image = global::MidProject.Properties.Resources.img;
+			this.pictureBox1.Location = new System.Drawing.Point(25, 122);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(83, 72);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::MidProject.Properties.Resources.Student_Management;
+			this.pictureBox2.Location = new System.Drawing.Point(926, 122);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(75, 72);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 7;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+			// 
+			// linkLabel6
+			// 
+			this.linkLabel6.AutoSize = true;
+			this.linkLabel6.Location = new System.Drawing.Point(494, 533);
+			this.linkLabel6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.linkLabel6.Name = "linkLabel6";
+			this.linkLabel6.Size = new System.Drawing.Size(80, 13);
+			this.linkLabel6.TabIndex = 8;
+			this.linkLabel6.TabStop = true;
+			this.linkLabel6.Text = "Student  Result";
+			this.linkLabel6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(-1, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1144, 100);
+			this.panel1.TabIndex = 9;
+			// 
+			// Home
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.PaleGreen;
+			this.ClientSize = new System.Drawing.Size(1160, 700);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.linkLabel6);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.linkLabel5);
+			this.Controls.Add(this.linkLabel4);
+			this.Controls.Add(this.linkLabel3);
+			this.Controls.Add(this.linkLabel2);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.pictureBox2);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Home";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
